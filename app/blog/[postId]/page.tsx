a40311e5a215fdab213cd11bd950255d0f7871fe
nino <god19124@gmail.com>
2024-04-30 00:46:47 +0900

    blog構築

---
+++ app/blog/[postId]/page.tsx
@@ -0,0 +1,34 @@
+import { notFound } from "next/navigation";
+import parse from "html-react-parser";
+import { getDetail, getList } from "@/libs/microcms";
+
+export async function generateStaticParams() {
+    const contents = await getList();
+
+    const paths = contents.map((post) => {
+        return {
+            postId: post.id,
+        };
+    });
+
+    return [...paths];
+}
+
+export default async function StaticDetailPage({
+    params: { postId },
+}: {
+    params: { postId: string };
+}) {
+    const post = await getDetail(postId);
+
+    if (!post) {
+        notFound();
+    }
+
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <div>{parse(post.body)}</div>
+        </div>
+    );
+}
---
+++ libs/client.ts
@@ -0,0 +1,7 @@
+// import { createClient } from "microcms-js-sdk";
+
+// export const client = createClient({
+//   // ドメイン名設定
+//   serviceDomain: "howto",
+//   apiKey: String(process.env.MICROCMS_API_KEY),
+// });
